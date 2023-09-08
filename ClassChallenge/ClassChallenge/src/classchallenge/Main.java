20ad35021816b105b432f1f68ae3874b602fb8bb
jonathan-christ <132149606+jonathan-christ@users.noreply.github.com>
2023-09-08 10:30:54 +0800

    1.1.0 Added Transfer Function

---
+++ ClassChallenge/ClassChallenge/src/classchallenge/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classchallenge;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+        public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        BankAccount Account = new BankAccount(1,100000,"Matthew Cedric D. Calaycay", "rufgier@gmail.com", "09696969696");
+        
+//        Account.setAccountNo(1);
+//        Account.setBal(0);
+//        Account.setName("Matthew Cedric D. Calaycay");
+//        Account.setEmail("rufgier@gmail.com");
+//        Account.setPhoneNo("123123123123"); //changes if not valid phone number (not implemented well)
+//        
+//        System.out.println(Account.getPhoneNo());
+//        System.out.println(Account.depositFunds(0) ? Account.getBal() : "Error");
+//        System.out.println(Account.withdrawFunds(0) ? Account.getBal() : "Error");
+//        
+//        System.out.println(Account.withdrawFunds(-100) ? Account.getBal() : "Error");
+//        System.out.println(Account.depositFunds(-100) ? Account.getBal() : "Error");
+//        
+//        System.out.println(Account.withdrawFunds(100) ? Account.getBal() : "Error");
+//        System.out.println(Account.depositFunds(100) ? Account.getBal() : "Error");
+//        System.out.println(Account.withdrawFunds(50) ? Account.getBal() : "Error");
+
+        BankAccount Account2 = new BankAccount(2,0, "Bitch", "asdiajs@gm.com", "09696969696");
+        
+            System.out.println(Account.getBal());
+            System.out.println(Account2.getBal());
+        Account2.transfer(Account, 500);
+        
+                    System.out.println(Account.getBal());
+            System.out.println(Account2.getBal());
+        
+        
+    }
+}
